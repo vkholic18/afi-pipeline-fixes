5c0ac0f47c1dcc176339262a6c2a4376774c1d5a
Development Team <dev@example.com>
2026-09-10 10:47:11 +0530

    refactor: remove duplicate GITHUB_API_KEY export and consolidate cleanup logic into cleanup_on_failure function

--- genctl-ci/onepipeline/pipelines/one_off/afi_terraform_merge/environment/secrets.sh
+++ genctl-ci/onepipeline/pipelines/one_off/afi_terraform_merge/environment/secrets.sh
@@ -12,7 +12,6 @@
 # Resolve ref:// secret references into env vars so get_env works for the rest
 set_env git-token "$(get_secret git-token)"
 
-export GITHUB_API_KEY=$(get_secret git-token)
 export GITHUB_API_KEY="$(get_secret git-token)"
 # used for terraform vars(override values without having to use an input)
 # https://developer.hashicorp.com/terraform/cli/config/environment-variables#tf_var_name
